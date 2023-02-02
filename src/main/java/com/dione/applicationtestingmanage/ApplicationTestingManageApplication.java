16bfd2969a6e2d91ead8dace78659057eba3aed6
dione <dioneousseynou03@gmail.com>
2023-02-02 21:41:23 +0100

    first commit

---
+++ src/main/java/com/dione/applicationtestingmanage/ApplicationTestingManageApplication.java
@@ -0,0 +1,13 @@
+package com.dione.applicationtestingmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTestingManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTestingManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dione/applicationtestingmanage/ApplicationTestingManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dione.applicationtestingmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTestingManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
